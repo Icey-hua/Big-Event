c88ee8195caff4d04c233890c279b362dfd9277a
xhh <263329019@qq.com>
2020-10-15 16:17:21 +0800

    完成主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,20 @@
 $.ajaxPrefilter(function (options) {
     // console.log(options);
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    // 以 /my 开头的请求路径，需要在请求头中携带 Authorization 身份认证字段，才能正常访问成功
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // bug:会先请求成功之后调用完成的回调函数再退出登录
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // console.log('失败');
+            localStorage.removeItem('token')
+            location.href = './login.html';
+
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    getUserinfo();
+    // 退出按钮
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function () {
+            // console.log(11);
+            localStorage.removeItem('token'); // 清除本地存储的请求头权限
+            location.href = '/login.html'; // 跳转到注册页面
+            layer.close(index);  // 关闭这个提示信息
+        })
+    })
+})
+// 获取用户基本信息
+function getUserinfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败!')
+            }
+            // 渲染头像
+            var data = res.data;
+            renderAvatar(data);
+        },
+        
+    })
+}
+// 渲染头像
+function renderAvatar(data) {
+    // 如果有头像图片，优先展示图片头像
+    if (data.user_pic != null) {
+        $('.layui-nav-img').src = data.user_pic;
+        $('.layui-nav-img').show()
+        $('.text-avatar').hide()
+    } else {
+        // 如果有文字，展示首字母大写,或中文首字符
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(data.username[0].toUpperCase() || data.username[0]); // 展示首字母
+        $('#welcome').html('你好!&nbsp;&nbsp;' + data.username)
+    }
+}  
